0a6b6ac1d7cdde40053e26e9ec8821c82de9e344
Mavagnale <mavagnale@gmail.com>
2023-07-02 18:19:45 +0200

    added estimation and control in a single thread

--- src/hierarchical_controller.cpp
+++ src/hierarchical_controller.cpp
@@ -33,41 +33,18 @@ const double Ts = 0.01;
 
 const double uncertainty = 1;
 const bool enable_estimator = 1;
+const bool enable_disturbance = 1;
 
-	//TEST////////////
-bool applyWrench( const geometry_msgs::Wrench& wrench) {
-    ros::NodeHandle nh;
-    ros::ServiceClient applyWrenchClient = nh.serviceClient<gazebo_msgs::ApplyBodyWrench>("/gazebo/apply_body_wrench");
-    gazebo_msgs::ApplyBodyWrench srv;
+const double init_prop_speed = 546;
+const double takeoff_time = 2;
 
-    srv.request.body_name = "firefly/base_link";
-    srv.request.reference_frame = "";
-    srv.request.reference_point.x = 0.0;
-    srv.request.reference_point.y = 0.0;
-    srv.request.reference_point.z = 0.0;
-    srv.request.wrench = wrench;
-    srv.request.start_time = ros::Time(0);
-    srv.request.duration = ros::Duration(0.01);
-
-    if (applyWrenchClient.call(srv) && srv.response.success)
-    {
-        ROS_INFO("Wrench applied successfully.");
-        return true;
-    }
-    else
-    {
-        ROS_ERROR("Failed to apply wrench.");
-        return false;
-    }
-}
-///////////
 
 
 class hierarchical_controller {
 	public:
 		hierarchical_controller();			
 		void ctrl_loop();
-		void estimator_loop();
+//		void estimator_loop();
         void run();
 
 
@@ -163,6 +140,7 @@ class hierarchical_controller {
 		Eigen::Matrix3d _Ke_dot;
 		Eigen::Matrix3d _Ki_e;
 
+		double _c0; //estimator constant
 
 
 		double _u_T;
@@ -224,6 +202,8 @@ hierarchical_controller::hierarchical_controller() : _pos_ref(0,0,-1) , _eta_ref
 	_Ke_dot << 10 , 0 , 0 , 0 , 10 , 0 , 0 , 0 , 5;
 	_Ki = 0.1;
 	_Ki_e << 0.1 , 0 , 0 , 0 , 0.1 , 0 , 0 , 0 , 0.05;
+
+	_c0 = 1;
 
 	_Q.setIdentity();
 	_Q_dot.setZero();
@@ -344,22 +324,55 @@ void hierarchical_controller::imu_callback ( sensor_msgs::Imu imu ){
 	_first_imu = true;
 }
 
-void hierarchical_controller::estimator_loop() {	
+
+bool applyWrench( const geometry_msgs::Wrench& wrench) {	//DISTURBANCE
+    ros::NodeHandle nh;
+    ros::ServiceClient applyWrenchClient = nh.serviceClient<gazebo_msgs::ApplyBodyWrench>("/gazebo/apply_body_wrench");
+    gazebo_msgs::ApplyBodyWrench srv;
+
+    srv.request.body_name = "firefly/base_link";
+    srv.request.reference_frame = "world";
+    srv.request.reference_point.x = 0.0;
+    srv.request.reference_point.y = 0.0;
+    srv.request.reference_point.z = 0.0;
+    srv.request.wrench = wrench;
+    srv.request.wrench.force.y = -wrench.force.y;
+    srv.request.wrench.force.z = -wrench.force.z;
+    srv.request.wrench.torque.y = -wrench.torque.y;
+    srv.request.wrench.torque.z = -wrench.torque.z;
+    srv.request.start_time = ros::Time::now();
+    srv.request.duration = ros::Duration(0.01);
+
+    if (applyWrenchClient.call(srv) && srv.response.success)
+    {
+    //    ROS_INFO("Wrench applied successfully.");
+        return true;
+    }
+    else
+    {
+        ROS_ERROR("Failed to apply wrench.");
+        return false;
+    }
+}
+///////////
+
+
+
+void hierarchical_controller::ctrl_loop() {	
+
 //G=k0^2/(k0+s)^2; transfer function for the estimator
 	ros::Rate rate(100);
-	double c0 = 1;
-	double k0 = c0;
+    mav_msgs::Actuators act_msg;
+    act_msg.angular_velocities.resize(motor_number);
+
+	Eigen::VectorXd angular_velocities_sq(motor_number);
+	angular_velocities_sq.setZero();
+	Eigen::Vector4d control_input;
+	control_input.setZero();
+
+	double k0 = _c0;
 	Eigen::Vector3d e3(0,0,1);
-
-
-
-	Eigen::Vector3d q_lin;
-	Eigen::Vector3d q_ang;
-
-	Eigen::Vector3d q_dot_lin_est;
-	Eigen::Vector3d q_dot_ang_est;
-	Eigen::Vector3d q_lin_est;
-	Eigen::Vector3d q_ang_est;
+	Eigen::Vector3d q_lin , q_ang , q_dot_lin_est , q_dot_ang_est , q_lin_est , q_ang_est;	
 	q_lin_est.setZero();
 	q_ang_est.setZero();		
 	_est_dist_lin.setZero();
@@ -368,60 +381,12 @@ void hierarchical_controller::estimator_loop() {
 	_u_T = 0;
 	_tau_b.setZero();
 
-	while(! _control_on) rate.sleep();
-	while(ros::ok){
-
-		q_lin = _mass*_p_b_dot;
-
-		q_ang = _M*_eta_b_dot;
-		
-		q_dot_lin_est = _est_dist_lin + _mass*gravity*e3 - _u_T*_Rb*e3;
-		q_dot_ang_est = _est_dist_ang + _C.transpose()*_eta_b_dot + _Q.transpose()*_tau_b;
-		
-		q_lin_est = q_lin_est + q_dot_lin_est*Ts;
-		q_ang_est = q_ang_est + q_dot_ang_est*Ts;
-
-		_est_dist_lin = k0*(q_lin - q_lin_est);
-		_est_dist_ang = k0*(q_ang - q_ang_est);
-
-		rate.sleep();
-	}
-
-}
-void hierarchical_controller::ctrl_loop() {	
-	ros::Rate rate(100);
-	std::string pkg_loc = ros::package::getPath("fsr_pkg");
-
 	Eigen::Vector3d e_p , e_p_dot, e_p_int , e_eta, e_eta_dot, e_eta_int , mu_d , tau_tilde ;
 	e_p_int.setZero();
 	e_eta_int.setZero();
-	_u_T = 0;
 
-	Eigen::VectorXd angular_velocities_sq(motor_number);
-	Eigen::VectorXd control_input(4);
-	control_input.setZero();
-    mav_msgs::Actuators act_msg;
-    act_msg.angular_velocities.resize(motor_number);
-
-
-	std::cout << "Waiting for sensors" << endl;
-	while (!_first_imu) rate.sleep();
-
-	std::cout << "Take off" << endl;
-	for (int i = 0 ; i < motor_number ; i++){
-	    act_msg.angular_velocities[i] = 545.1;	
-	}
-
-	_act_pub.publish(act_msg);
-
-	sleep(5.0);
-	std::cout << "Control on" << endl;
-	_control_on = true;
-
-	Eigen::Vector3d ang_dist = Eigen::Vector3d(0.0,0.0,0.0);	//disturbance
 
 	double phi_ref , theta_ref , phi_ref_dot, theta_ref_dot , phi_ref_dot_dot , theta_ref_dot_dot , phi_ref_old = 0 , theta_ref_old = 0 , phi_ref_dot_old = 0 , theta_ref_dot_old = 0 ;
-
 	float phi_ref_dot_dot_f = 0.0;
 	float theta_ref_dot_dot_f = 0.0;
 	float phi_ref_dot_dot_old_f = 0.0;
@@ -429,139 +394,183 @@ void hierarchical_controller::ctrl_loop() {
 	float phi_ref_dot_dot_old = 0.0;
 	float theta_ref_dot_dot_old = 0.0;	
 	
+
+
+	cout << "_est_dist_lin" << _est_dist_lin << endl;
+	cout << "_est_dist_ang" << _est_dist_ang << endl;
+
+	cout << "q_dot_lin_est" << q_dot_lin_est << endl;
+	cout << "q_dot_ang_est" << q_dot_ang_est << endl;
+
+
+	cout << "Waiting for sensors" << endl;
+	while( !(_first_imu && _first_odom) ) rate.sleep();
+
+
+
+	double time = 0;
+
+	std::cout << "Take off" << endl;
+
+
+
 	while(ros::ok){
 
-
-		cout << "p_b:" << _p_b << endl << endl;
-		cout << "p_b_dot:" << _p_b_dot << endl << endl;
-		cout << "pos_ref:" << _pos_ref << endl << endl;
-		cout << "e_p: " << endl << e_p << endl << endl;
-
-		cout << "omega_b_b:" << _omega_b_b << endl << endl;
-		cout << "eta_b:" << _eta_b << endl << endl;
-		cout << "eta_b_dot:" << _eta_b_dot << endl << endl;
-		cout << "_est_dist_lin: " << endl << _est_dist_lin << endl;
-		cout << "_est_dist_ang: " << endl << _est_dist_ang << endl << endl;
-		cout << "control input: " << endl << control_input << endl << endl;
-
-
-		e_p = _p_b - _pos_ref;
-		e_p_dot = _p_b_dot - _pos_ref_dot;
-
-		e_p_int = e_p_int + e_p*Ts;
 		
+		if (enable_estimator){						//ESTIMATION
+			q_lin = _mass*_p_b_dot;
+			q_ang = _M*_eta_b_dot;
+			
+			q_dot_lin_est = _est_dist_lin + _mass*gravity*e3 - _u_T*_Rb*e3;
+			q_dot_ang_est = _est_dist_ang + _C.transpose()*_eta_b_dot + _Q.transpose()*_tau_b;
+			
+			q_lin_est = q_lin_est + q_dot_lin_est*Ts;
+			q_ang_est = q_ang_est + q_dot_ang_est*Ts;
 
-		mu_d = -_Kp*e_p -_Kp_dot*e_p_dot -_Ki*e_p_int  + _pos_ref_dot_dot - _est_dist_lin/_mass;
-		_u_T = _mass*sqrt(mu_d(0)*mu_d(0) + mu_d(1)*mu_d(1) + (mu_d(2)-gravity)*(mu_d(2)-gravity));
+			_est_dist_lin = k0*(q_lin - q_lin_est);
+			_est_dist_ang = k0*(q_ang - q_ang_est);
 
-
-
-		phi_ref = asin( _mass/_u_T*( mu_d(1)*cos(_eta_ref(2)) - mu_d(0)*sin(_eta_ref(2)) ) );
-		phi_ref_dot = (phi_ref-phi_ref_old)/Ts;  // Ts=0.01
-		phi_ref_dot_dot = (phi_ref_dot-phi_ref_dot_old)/Ts;  // Ts=0.01
-
-
-		phi_ref_dot_dot_f = 0.9048*phi_ref_dot_dot_old_f + phi_ref_dot_dot_old*0.009516;  	//frequenza di taglio a 10 hz
-		phi_ref_dot_dot_old_f = phi_ref_dot_dot_f;
-		phi_ref_dot_dot_old = phi_ref_dot_dot;
-
-
-		theta_ref = atan((mu_d(0)*cos(_eta_ref(2)) + mu_d(1)*sin(_eta_ref(2)))/(mu_d(2)-gravity));
-		theta_ref_dot = (theta_ref-theta_ref_old)/Ts;  // Ts=0.01
-		theta_ref_dot_dot = (theta_ref_dot-theta_ref_dot_old)/Ts;  // Ts=0.01
-
-
-		theta_ref_dot_dot_f = 0.9048*theta_ref_dot_dot_old_f + theta_ref_dot_dot_old*0.009516;  	//frequenza di taglio a 10 hz
-		theta_ref_dot_dot_old_f = theta_ref_dot_dot_f;
-		theta_ref_dot_dot_old = theta_ref_dot_dot;		
-
-
-
-
-
-
-		_eta_ref(0) = phi_ref;
-		_eta_ref(1) = theta_ref;
-		_eta_ref_dot(0) = phi_ref_dot;
-		_eta_ref_dot(1) = theta_ref_dot;
-		_eta_ref_dot_dot(0) = phi_ref_dot_dot_f;
-		_eta_ref_dot_dot(1) = theta_ref_dot_dot_f;				
-
-		e_eta = _eta_b - _eta_ref;
-		e_eta_dot = _eta_b_dot - _eta_ref_dot;
-		e_eta_int = e_eta_int + e_eta*Ts;
-
-		tau_tilde =  -_Ke*e_eta - _Ke_dot*e_eta_dot - _Ki_e*e_eta_int + _eta_ref_dot_dot;
-
-		Eigen::Vector3d _tau_b_real;
-		_tau_b = _Ib*_Q*tau_tilde + (_Q.transpose()).inverse()*_C*_eta_b_dot -(_Q.transpose()).inverse()*_est_dist_ang;
-		_tau_b_real = _tau_b + ang_dist;
-		geometry_msgs::Wrench wrench_msg;
-		wrench_msg.force.x = 0;
-		wrench_msg.force.y = 0;
-		wrench_msg.force.z = -_u_T;
-		wrench_msg.torque.x = _tau_b(0);
-		wrench_msg.torque.y = _tau_b(1);
-		wrench_msg.torque.z = _tau_b(2);
-		_wrench_pub.publish(wrench_msg);
-
-		phi_ref_old = phi_ref;
-		theta_ref_old = theta_ref;
-		phi_ref_dot_old = phi_ref_dot;
-		theta_ref_dot_old = theta_ref_dot;		
-
-		control_input(0) = _u_T;
-		control_input(1) = _tau_b_real(0);
-		control_input(2) = _tau_b_real(1);
-		control_input(3) = _tau_b_real(2);
-	
-		angular_velocities_sq =  _allocation_matrix.transpose()*(_allocation_matrix*_allocation_matrix.transpose()).inverse() * control_input;
-
-		for (int i=0 ; i<motor_number ; i++){
-		//	std::cout << angular_velocities_sq(i) << endl;
-			if (angular_velocities_sq(i) >= 0) {
-		    	act_msg.angular_velocities[i] = sqrt(angular_velocities_sq(i));	
-			}
-			if (angular_velocities_sq(i) < 0) {
-				cout << "negative motor velocity!" << endl;
-		    	act_msg.angular_velocities[i] = 0;
-			}
+			cout << "_est_dist_lin: " << endl << _est_dist_lin << endl;
+			cout << "_est_dist_ang: " << endl << _est_dist_ang << endl << endl;
 		}
-			//FOR DATA LOGGING
 
-		if (_first_ref == true &&  _log_time < 60){
-			log_data();			
-			_log_time = _log_time + Ts;	
-		}	
-
-		_act_pub.publish(act_msg);
-
-		// Create a wrench with the desired force and torque values
-		geometry_msgs::Vector3 force;
-		force.x = 10.0;
-		force.y = 0.0;
-		force.z = 0.0;
-
-		geometry_msgs::Vector3 torque;
-		torque.x = 0.0;
-		torque.y = 0.0;
-		torque.z = 0.0;
-
-		geometry_msgs::Wrench wrench;
-		wrench.force = force;
-		wrench.torque = torque;
-		applyWrench(wrench);
+		if (time <= takeoff_time){						//TAKE OFF
+			for (int i = 0 ; i < motor_number ; i++){
+	    		act_msg.angular_velocities[i] = init_prop_speed;	
+			}
+			_u_T = pow(init_prop_speed,2)*C_T*motor_number;
+			_act_pub.publish(act_msg);	
+		}
 
 
+		if (time > takeoff_time){						//CONTROL LOOP
+		/*
+			cout << "p_b:" << _p_b << endl << endl;
+			cout << "p_b_dot:" << _p_b_dot << endl << endl;
+			cout << "pos_ref:" << _pos_ref << endl << endl;
+			cout << "e_p: " << endl << e_p << endl << endl;
+
+			cout << "omega_b_b:" << _omega_b_b << endl << endl;
+			cout << "eta_b:" << _eta_b << endl << endl;
+			cout << "eta_b_dot:" << _eta_b_dot << endl << endl;
+			cout << "control input: " << endl << control_input << endl << endl;
+			*/
+
+			e_p = _p_b - _pos_ref;
+			e_p_dot = _p_b_dot - _pos_ref_dot;
+
+			e_p_int = e_p_int + e_p*Ts;
+			
+
+			mu_d = -_Kp*e_p -_Kp_dot*e_p_dot -_Ki*e_p_int  + _pos_ref_dot_dot - _est_dist_lin/_mass;
+			_u_T = _mass*sqrt(mu_d(0)*mu_d(0) + mu_d(1)*mu_d(1) + (mu_d(2)-gravity)*(mu_d(2)-gravity));
 
 
-		rate.sleep();		
+
+			phi_ref = asin( _mass/_u_T*( mu_d(1)*cos(_eta_ref(2)) - mu_d(0)*sin(_eta_ref(2)) ) );
+			phi_ref_dot = (phi_ref-phi_ref_old)/Ts;  // Ts=0.01
+			phi_ref_dot_dot = (phi_ref_dot-phi_ref_dot_old)/Ts;  // Ts=0.01
+
+
+			phi_ref_dot_dot_f = 0.9048*phi_ref_dot_dot_old_f + phi_ref_dot_dot_old*0.009516;  	//frequenza di taglio a 10 hz
+			phi_ref_dot_dot_old_f = phi_ref_dot_dot_f;
+			phi_ref_dot_dot_old = phi_ref_dot_dot;
+
+
+			theta_ref = atan((mu_d(0)*cos(_eta_ref(2)) + mu_d(1)*sin(_eta_ref(2)))/(mu_d(2)-gravity));
+			theta_ref_dot = (theta_ref-theta_ref_old)/Ts;  // Ts=0.01
+			theta_ref_dot_dot = (theta_ref_dot-theta_ref_dot_old)/Ts;  // Ts=0.01
+
+
+			theta_ref_dot_dot_f = 0.9048*theta_ref_dot_dot_old_f + theta_ref_dot_dot_old*0.009516;  	//frequenza di taglio a 10 hz
+			theta_ref_dot_dot_old_f = theta_ref_dot_dot_f;
+			theta_ref_dot_dot_old = theta_ref_dot_dot;		
+
+
+
+			_eta_ref(0) = phi_ref;
+			_eta_ref(1) = theta_ref;
+			_eta_ref_dot(0) = phi_ref_dot;
+			_eta_ref_dot(1) = theta_ref_dot;
+			_eta_ref_dot_dot(0) = phi_ref_dot_dot_f;
+			_eta_ref_dot_dot(1) = theta_ref_dot_dot_f;				
+
+			e_eta = _eta_b - _eta_ref;
+			e_eta_dot = _eta_b_dot - _eta_ref_dot;
+			e_eta_int = e_eta_int + e_eta*Ts;
+
+			tau_tilde =  -_Ke*e_eta - _Ke_dot*e_eta_dot - _Ki_e*e_eta_int + _eta_ref_dot_dot;
+
+			Eigen::Vector3d _tau_b_real;
+			_tau_b = _Ib*_Q*tau_tilde + (_Q.transpose()).inverse()*_C*_eta_b_dot -(_Q.transpose()).inverse()*_est_dist_ang;
+
+
+		/*	geometry_msgs::Wrench wrench_msg;
+			wrench_msg.force.x = 0;
+			wrench_msg.force.y = 0;
+			wrench_msg.force.z = -_u_T;
+			wrench_msg.torque.x = _tau_b(0);
+			wrench_msg.torque.y = _tau_b(1);
+			wrench_msg.torque.z = _tau_b(2);
+			_wrench_pub.publish(wrench_msg);
+		
+		*/
+			phi_ref_old = phi_ref;
+			theta_ref_old = theta_ref;
+			phi_ref_dot_old = phi_ref_dot;
+			theta_ref_dot_old = theta_ref_dot;		
+
+			control_input(0) = _u_T;
+			control_input(1) = _tau_b(0);
+			control_input(2) = _tau_b(1);
+			control_input(3) = _tau_b(2);
+		
+			angular_velocities_sq =  _allocation_matrix.transpose()*(_allocation_matrix*_allocation_matrix.transpose()).inverse() * control_input;
+
+			for (int i=0 ; i<motor_number ; i++){
+			//	std::cout << angular_velocities_sq(i) << endl;
+				if (angular_velocities_sq(i) >= 0) {
+					act_msg.angular_velocities[i] = sqrt(angular_velocities_sq(i));	
+				}
+				if (angular_velocities_sq(i) < 0) {
+					cout << "negative motor velocity!" << endl;
+					act_msg.angular_velocities[i] = 0;
+				}
+			}
+				//FOR DATA LOGGING
+
+			if (_first_ref == true &&  _log_time < 60){
+				log_data();			
+				_log_time = _log_time + Ts;	
+			}	
+
+			_act_pub.publish(act_msg);
+		}
+
+		if (enable_disturbance){
+			geometry_msgs::Vector3 force , torque;
+			force.x = 0.2;
+			force.y = 0.3;
+			force.z = 0.0;
+			torque.x = 0.0;
+			torque.y = 0.0;
+			torque.z = 0.02;
+			geometry_msgs::Wrench wrench;
+			wrench.force.x = force.x*std::min(time/takeoff_time , 1.0);
+			wrench.force.y = force.y*std::min(time/takeoff_time , 1.0);
+			wrench.force.z = force.z*std::min(time/takeoff_time , 1.0);
+			wrench.torque.x = torque.x*std::min(time/takeoff_time , 1.0);
+			wrench.torque.y = torque.y*std::min(time/takeoff_time , 1.0);
+			wrench.torque.z = torque.z*std::min(time/takeoff_time , 1.0);			
+			applyWrench(wrench);
+		}
+
+
+		time = time + Ts;
+		rate.sleep();
 	}
+
 }
-
-
-
-
 
 
 void hierarchical_controller::empty_txt(){
@@ -763,9 +772,6 @@ void hierarchical_controller::log_data(){
 
 void hierarchical_controller::run() {
 	boost::thread ctrl_loop_t ( &hierarchical_controller::ctrl_loop, this);
-	if (enable_estimator){
-		boost::thread estimator_loop_t ( &hierarchical_controller::estimator_loop, this);
-	}
 	ros::spin();	
 }
 
