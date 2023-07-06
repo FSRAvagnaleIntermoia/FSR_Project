66f56a43b66c7b04e32a71961192a1d9e7b13ced
Mavagnale <80157936+Mavagnale@users.noreply.github.com>
2023-07-06 12:16:37 +0200

    fixed indentation

--- src/actuator_publisher.cpp
+++ src/actuator_publisher.cpp
@@ -10,10 +10,10 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv,"actuator_publisher");
 	ros::NodeHandle nh;
 	ros::Publisher cmd_pub = nh.advertise<mav_msgs::Actuators>("/firefly/command/motor_speed", 1);
-
-    mav_msgs::Actuators act_msg;
-    act_msg.angular_velocities.resize(prop_number);
-
+	
+	mav_msgs::Actuators act_msg;
+	act_msg.angular_velocities.resize(prop_number);
+	
 	float ang_vel;
     
 	ros::Rate rate(10); 
@@ -21,14 +21,14 @@ int main(int argc, char **argv) {
 		cout << "Insert motor speed" << endl;
 		cin >> ang_vel;
 		
-	    act_msg.angular_velocities[0] = ang_vel;	
-	    act_msg.angular_velocities[1] = ang_vel;	
-	    act_msg.angular_velocities[2] = ang_vel;	
-	    act_msg.angular_velocities[3] = ang_vel;	
-	    act_msg.angular_velocities[4] = ang_vel;	
-	    act_msg.angular_velocities[5] = ang_vel;	
-
-
+		act_msg.angular_velocities[0] = ang_vel;	
+		act_msg.angular_velocities[1] = ang_vel;	
+		act_msg.angular_velocities[2] = ang_vel;	
+		act_msg.angular_velocities[3] = ang_vel;	
+		act_msg.angular_velocities[4] = ang_vel;	
+		act_msg.angular_velocities[5] = ang_vel;	
+		
+		
 		cmd_pub.publish(act_msg);
 		rate.sleep();		
 	}
